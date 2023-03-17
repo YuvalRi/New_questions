bf3c11b27f0c1f3c2efbc0735443f9f43a926697
Yuval <yuvalritsker@gmail.com>
2023-03-17 17:59:10 +0200

    all basics exercises

--- Task_1.py
+++ Task_1.py
@@ -1,9 +1,48 @@
 # Task 1
 
-def x_s():
+# 1
+def right_angle():
     for i in range(1, 6):
         print("X "*i)
 
+# 2
+def isosceles_triangle():
+    for i in range(7, 0, -2):
+        spaces = "  " * ((7 - i) // 2)
+        print(spaces + "X " * i)
+
+# 3 
+def triangle():
+    right_angle()
+    for i in range(4, 0, -1):
+        print("X "* i)
+
+# 4
+def steps_to_n(n: int):
+    if n <= 0:
+        raise ValueError("Error! Input must be a natural number")
+    for i in range(1, n+1):
+        for j in range(1, i+1):
+            print(j, end =' ')
+        print()
+
+# 5
+def odd_and_even_sequence(n: int):
+    if n <= 0:
+        raise ValueError("Error! Input must be a natural number")
+    for i in range(1, n+1):
+        for j in range(1, i+1):
+            if (i % 2 != 0):
+                print(2*j-1, end =' ')
+            else:
+                print(2*j, end =' ')
+        print()
+
 if __name__ == "__main__":
-    x_s()
+    right_angle()
+    isosceles_triangle()
+    triangle()
+    steps_to_n(n=0)
+    steps_to_n(n=5)
+    odd_and_even_sequence(n=7)
 
